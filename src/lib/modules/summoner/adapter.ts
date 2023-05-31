00f25e321b14453e87b5d93d241ef5e5436e6df6
Aymane <aymane@cashflowpositifi.fr>
2023-05-31 13:55:49 +0200

    fix updating of revision date when revision date has expired

--- src/lib/modules/summoner/adapter.ts
+++ src/lib/modules/summoner/adapter.ts
@@ -4,9 +4,20 @@ import type { SummonerDTO } from "@/lib/models/summoner";
 import type { Region } from "@/lib/constants/platforms";
 
 export function summonerToPrismaSummoner(summonerDTO: SummonerDTO, region: Region){
+
+    const today = moment();
+    // Add 1 week to the current date
+    const nextWeek = today.add(1, 'week');
+    // Get the timestamp for next week
+    const timestamp = nextWeek.unix();
+    
+    summonerDTO.revisionDate = timestamp;
+    console.log('summonerDTO revision date ' + summonerDTO.revisionDate)
     return Prisma.validator<Prisma.SummonerCreateInput>()({
         ...summonerDTO,
-        revisionDate: moment(summonerDTO.revisionDate).toDate(),
+        revisionDate: moment.unix(summonerDTO.revisionDate).toDate(),
         region
     })
 }
+
+// moment.unix((data as SummonerDTO).revisionDate).format('ddd MMM DD YYYY HH:mm:ss'))
